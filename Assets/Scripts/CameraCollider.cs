309d025309f72e0004d36ff19d3fb2c4601031ea
MiskaRantala <miska.rantala@gmail.com>
2018-03-21 00:01:27 +0200

    Assignment

---
+++ Assets/Scripts/CameraCollider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraCollider : MonoBehaviour
+{
+    const float MIN_DISTANCE = 25f;
+
+    public Transform Player; 
+    public float speed = 1;
+    private Vector3 destination;
+    private Vector3 projection;
+
+    void Start()
+    {
+        destination = transform.position;
+        projection = Player.position;
+    }
+
+    void LateUpdate()
+    {
+        if ((Player.position - projection).magnitude < MIN_DISTANCE)
+        {
+            projection = Vector3.MoveTowards(projection, Player.position, Time.deltaTime * speed);
+            destination = projection;
+            destination.y = transform.position.y;
+            transform.position = destination;
+        }
+    }
+}
